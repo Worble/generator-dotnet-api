7c40f7cfe603aac63a1bb202636b83ed3e224e0d
Robert Clarke <Robert.Clarke@blacklightsoftware.com>
2020-01-15 12:55:17 +0000

    added path to options

--- generators/dotnet/index.ts
+++ generators/dotnet/index.ts
@@ -88,6 +88,12 @@ interface IGeneratorPrompts {
 }
 
 module.exports = class extends Generator {
+  initializing() {
+    if (this.options.path) {
+      this.destinationRoot(this.destinationPath(this.options.path));
+    }
+  }
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(
